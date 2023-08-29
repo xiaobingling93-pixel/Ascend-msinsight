b1633fdb8060e5ed8a1ade488eb8764ed8cb9b6a
insightDeveloper <1390732074@qq.com>
2023-08-29 09:25:45 +0800

    中心server
    Match-id-6b2388fadf4e1605f960d4a99ed14209472bb36e

---
+++ src/centralServer/websocket/messageManager.ts
@@ -0,0 +1,33 @@
+import type { DataRequest, ModuleName, NotificationRegistration, Remote } from './defs';
+import { Connection } from '@/centralServer/websocket/connection';
+
+export const CONNECTION_MAP: Map<Remote, Connection> = new Map();
+
+export const NOTIFICATION_METHOD_MAP: Map<ModuleName, Function> = new Map();
+
+export const registerNotification = function (notificationRegistration: NotificationRegistration) {
+    NOTIFICATION_METHOD_MAP.set(
+        notificationRegistration.moduleName,
+        notificationRegistration.callBack,
+    );
+};
+
+export const connectRemote = async function (remote: Remote): Promise<boolean> {
+    const connection = new Connection(remote);
+    try {
+        await connection.connect();
+    } catch (e) {
+        return false;
+    }
+    CONNECTION_MAP.set(remote, connection);
+    return true;
+};
+
+export const request = async function (
+    remote: Remote,
+    moduleName: ModuleName,
+    args: DataRequest,
+): Promise<unknown> {
+    const connection: Connection = CONNECTION_MAP.get(remote);
+    return await connection.fetch(moduleName, args);
+};
---
+++ src/platform/messageSender.ts
@@ -0,0 +1,35 @@
+import { Browser } from './Browser';
+import { IntellijPlatform } from './IntellijPlatform';
+import { VsCodePlatform } from './VsCodePlatform';
+
+declare function acquireVsCodeApi(): any;
+
+export let messageSender: IMessageSender = new Browser();
+
+if (typeof acquireVsCodeApi === 'function') {
+    messageSender = new VsCodePlatform();
+    messageSender.sendMessage = acquireVsCodeApi().postMessage;
+}
+
+if (typeof window.cefQuery === 'function') {
+    messageSender = new IntellijPlatform();
+}
+
+export interface IMessageSender {
+    sendMessage: (ceq: any) => void;
+    selectFolder: () => Promise<string>;
+    selectFile: () => Promise<string>;
+}
+
+export const removeAndAddEventListener = (resolve: (value: (string | PromiseLike<string>)) => void): void => {
+    function onMessage(event: MessageEvent): void {
+        const message = event.data;
+        if (message.command === 'ascend.folderSelected') {
+            resolve(message.path);
+        } else if (message.command === 'ascend.folderSelectionCanceled') {
+            resolve('');
+        }
+    }
+    window.removeEventListener('message', onMessage);
+    window.addEventListener('message', onMessage);
+};
