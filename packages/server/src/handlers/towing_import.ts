46a7e54e1a09492b9092aabe2a6d8e4da8cdb4ae
insightDeveloper <1390732074@qq.com>
2023-09-05 09:12:00 +0800

    ******** 集中式场景下支持本地文件拖入数据后端解析,分片导入完善
    Match-id-814708d9ccd9fad18938505f9f605b1a3e3cffa6

--- packages/server/src/handlers/towing_import.ts
+++ packages/server/src/handlers/towing_import.ts
@@ -18,15 +18,20 @@ export type ImportRequest = {
     buffer: any;
     isInFolder: boolean;
     isLast: boolean;
+    slice: {
+        isSlice: boolean;
+        index: number;
+        count: number;
+    };
 };
 
 export const towingImportHandler = async (request: ImportRequest, client: Client): Promise<Record<string, unknown>> => {
     logger.info('接收请求，开始下载文件');
-    const { buffer, isLast, name, path } = request;
+    const { buffer, isLast, name, path, isInFolder } = request;
     const realPath = path.split(name)[0].toString();
     let data = Buffer.alloc(0);
     data = Buffer.concat([ data, buffer ], data.length + buffer.length);
-    const downLoadPath = join(__dirname, 'download', realPath);
+    const downLoadPath = isInFolder ? join(__dirname, 'download') : join(__dirname, 'download', realPath);
     fs.mkdir(downLoadPath, { recursive: true }, (err, path) => {
         if (err) {
             return logger.error('创建文件夹失败');
@@ -35,17 +40,29 @@ export const towingImportHandler = async (request: ImportRequest, client: Client
         }
     });
     // 需要接收一个flag 来识别是否为切片文件
-    fs.writeFile(join(__dirname, 'download', path), data, { flag: 'w+' }, function(err) {
-        if (err) {
-            return logger.error('文件下载成功');
-        } else {
-            logger.info('文件下载成功');
-        }
-    });
+    const isSlice: boolean = request.slice.isSlice;
+    const sliceIndex: number = request.slice.index;
+    if (isSlice && sliceIndex > 1) {
+        fs.appendFile(join(__dirname, 'download', path), data, { flag: 'w+' }, function(err) {
+            if (err) {
+                return logger.error('文件追加失败');
+            } else {
+                logger.info('文件追加成功');
+            }
+        });
+    } else {
+        fs.writeFile(join(__dirname, 'download', path), data, { flag: 'w+' }, function(err) {
+            if (err) {
+                return logger.error('文件下载失败');
+            } else {
+                logger.info('文件下载成功');
+            }
+        });
+    }
+
     if (isLast) {
         logger.info('开始解析文件');
-        const result = parseFile(downLoadPath, client);
-        return await result;
+        return await parseFile(downLoadPath, client);
     }
     return { msg: '文件下载还在继续', isEnd: false };
 };
