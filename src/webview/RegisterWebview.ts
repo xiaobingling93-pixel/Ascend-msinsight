084eb1877701ad269e2b19dac47a14cb71f8bbaf
insightDeveloper <1390732074@qq.com>
2023-09-06 14:17:17 +0800

    新增启动server和通信逻辑
    Match-id-6f0465ea59d99c528b107d0eb80d2123a8d1147f

--- src/webview/RegisterWebview.ts
+++ src/webview/RegisterWebview.ts
@@ -1,21 +1,30 @@
 import * as vscode from 'vscode';
 import { Webview } from "./Webview";
+import { spawn, exec, ChildProcess} from 'child_process';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import { platform } from 'os';
+import { WebviewManager } from './WebviewManager';
 
 export class RegisterWebview extends Webview {
 
     private readonly _extensionUri: vscode.Uri;
 
     private readonly nameList: any;
+    private server?: ChildProcess;
+    private serverCheckSchedule?: NodeJS.Timeout;
+    private tryRestartTime = 0;
+    private readonly port = 9001;
+    private readonly findServerCommand = platform() === 'win32' ? 'tasklist | findstr profiler_server.exe' : 'ps aux | grep profiler_serve';
 
-    constructor(viewType: string, title: string, context: vscode.ExtensionContext) {
-        super(viewType, title, context);
+    constructor(viewType: string, title: string, context: vscode.ExtensionContext,  manager: WebviewManager) {
+        super(viewType, title, context, manager);
         this._extensionUri = context.extensionUri;
         let data = readFileSync(join(__dirname, './profiler/manifest.json'));
         let result = data.toString();
         const jsonObject = JSON.parse(result);
         this.nameList = jsonObject;
+        this.startServer();
     }
 
     newPanel() {
@@ -40,11 +49,59 @@ export class RegisterWebview extends Webview {
         (this.panel as vscode.WebviewPanel).webview.html = this.html();
     }
 
-    dispose() {
-        console.log('profiler is closed!');
+    startServer() {
+        this.executeStartServerCommand();
+        this.startServerCheckAndRestart();
     }
 
-    // rem单位相对于font-size取值，设为6px，设计稿为750px时，设置初始UI宽度缩放100%时450px，6*750/450=10px=1rem，默认缩放比例为75%，初始时6*0.75=4.5
+    executeStartServerCommand() {
+        let serverName = './profiler/server/profiler_server';
+        if (platform() === 'win32') {
+            serverName = serverName + '.exe';
+        }
+        const serverPath = join(__dirname, serverName);
+        if (platform() !== 'win32') {
+            spawn('chmod', ['+x', serverPath]);
+        }
+        const serverCommand = serverPath;
+        this.server = spawn(serverCommand, [' --wsPort=' + this.port]);
+        this.server.stdout?.on('data', (data: any) => {
+            console.log('[server][info]: ' + data);
+        });
+        this.server.stderr?.on('data', (data: any) => {
+            console.log('[server][err]: ' + data);
+        });
+    }
+
+    startServerCheckAndRestart() {
+        this.serverCheckSchedule = setInterval(() => {
+            exec(this.findServerCommand, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                }
+                if (stderr) {
+                    console.error(`exec stderr: ${stderr}`);
+                }
+                // server挂了,只提示3次
+                if (!stdout.includes('profiler_server')) {
+                    if (this.tryRestartTime++ < 3) {
+                        vscode.window.showWarningMessage('[insight]: server has been dead, please close and reopen');
+                        return;
+                    } 
+                    clearInterval(this.serverCheckSchedule);
+
+                }
+            });
+        }, 3000);
+    }
+
+    dispose() {
+        console.log('profiler is closed!');
+        clearInterval(this.serverCheckSchedule);
+        this.server?.kill();
+        this.webviewManger.dispose();
+    }
+
     html() {
         return `<!DOCTYPE html>
                     <html lang="en">
@@ -54,7 +111,7 @@ export class RegisterWebview extends Webview {
                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
                         <title>Vite App</title>
                         <link rel="stylesheet" href="${this.style()}">
-                        <script type="module" src="${this.entry()}"></script>
+                        <script defer="defer" src="${this.entry()}"></script>
                       </head>
                       <body>
                         <div id="app"></div>
