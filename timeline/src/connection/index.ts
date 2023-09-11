52ae9752793345b086f4274887a5c58b4dca10fb
insightDeveloper <1390732074@qq.com>
2023-09-11 15:05:50 +0800

    adapt connection with framework
    Match-id-d16bb8ecddb074b525878ec42e7a3a3b961bd3e6

---
+++ timeline/src/connection/index.ts
@@ -0,0 +1,143 @@
+type TargetWindow = Window | null | undefined;
+type ReservedEventHandler = 'request';
+type EventHanlder = string;
+type SendParams<T extends EventHanlder> = {
+    event: T extends ReservedEventHandler ? never : T;
+    [x: string]: unknown;
+};
+type ListenerCallback = (res: MessageEvent) => void;
+abstract class BaseConnector {
+    protected readonly _errMsgType = 'postMessage';
+    protected invalidFunc: null | (() => void) = null;
+    protected _targetWindow: TargetWindow;
+    protected _getTargetWindow: () => TargetWindow;
+    protected _listeners: Map<EventHanlder, ListenerCallback> = new Map();
+    protected _broadcastListeners: ListenerCallback[] = [];
+
+    constructor(getTargetWindow: () => TargetWindow) {
+        if (typeof window !== 'object') {
+            const errMsg = 'cannot find global Window object, please check your runtime environment';
+            console.error(this.printErrMsg(errMsg));
+            this.invalidFunc = () => { throw new Error(this.printErrMsg(errMsg)); };
+        }
+        this._getTargetWindow = getTargetWindow;
+
+        window.onmessage = (event: MessageEvent) => {
+            const res = { ...event };
+            res.data = JSON.parse(event.data);
+            const listener = this._listeners.get(res.data.event);
+            if (res.data.event === 'request') {
+                this.awaitFetch(res);
+            } else if (res.data.event === 'broadcast') {
+                this._broadcastListeners.forEach(callback => callback(res));
+            } else if (listener) {
+                listener(res);
+            } else {
+                console.warn(this.printErrMsg('missed [event] in your message, please check your params, or maybe have an invalid send'));
+            }
+        };
+        Object.defineProperty(window, 'onmessage', {
+            configurable: false,
+            set: () => {
+                console.warn(this.printErrMsg('the property \'window.onmessage\' in used, reassign it is invalid'));
+            },
+        });
+    }
+
+    protected printErrMsg(errMsg: string): string {
+        return `[${this._errMsgType}]: ${errMsg}`;
+    }
+
+    protected abstract awaitFetch(e: MessageEvent): void;
+
+    send<T extends EventHanlder>(params: SendParams<T>, reject?: Function): void {
+        if (this.invalidFunc) {
+            this.invalidFunc();
+            return;
+        }
+        if (!this._targetWindow) {
+            this._targetWindow = this._getTargetWindow();
+        }
+        if (!this._targetWindow?.postMessage) {
+            const errMsg = 'missed postMessage function, please check your iframe element';
+            console.warn(this.printErrMsg(errMsg));
+            reject?.(new Error(errMsg));
+            return;
+        }
+        this._targetWindow.postMessage(JSON.stringify(params), '*');
+    }
+
+    addListener<T extends EventHanlder>(event: T extends ReservedEventHandler ? never : T, callback: ListenerCallback): T {
+        if (this.invalidFunc) {
+            this.invalidFunc();
+        }
+        if (this._listeners.has(event)) {
+            console.warn(this.printErrMsg('detected some even type was overrided, this maybe cause some mistake, pleas check it'));
+        }
+        this._listeners.set(event, callback);
+        return event;
+    }
+
+    removeListener<T extends EventHanlder>(event: T extends ReservedEventHandler ? never : T): void {
+        if (this.invalidFunc) {
+            this.invalidFunc();
+        }
+        this._listeners.delete(event);
+    }
+};
+
+type FetchSequenceID = number;
+type Response = (res: MessageEvent) => Promise<Record<string, unknown>>;
+class ServerConnector extends BaseConnector {
+    private _responseForFetch: Response | null = null;
+
+    resigsterAwaitFetch(callback: Response): void {
+        this._responseForFetch = callback;
+    }
+
+    protected async awaitFetch(event: MessageEvent): Promise<void> {
+        if (typeof event.data.id !== 'number' || !this._responseForFetch) {
+            const errMsg = 'something wrong with requset response for fetch listener or data.id, please check your config';
+            console.error(this.printErrMsg(errMsg));
+            return;
+        }
+        const res = await this._responseForFetch(event);
+        this.send({ event: 'request', id: event.data.id, ...res } as SendParams<EventHanlder>);
+    }
+};
+
+class ClientConnector extends BaseConnector {
+    private readonly _msgSequence: Map<FetchSequenceID, Function> = new Map();
+    private _curFetchSequenceID: FetchSequenceID = 0;
+
+    protected awaitFetch(res: MessageEvent): void {
+        const _resolve = this._msgSequence.get(res.data.id);
+        if (!_resolve) {
+            console.warn(this.printErrMsg(
+                'cannot find relative resolve for this fetch, this maybe cause omission of message, please check your connection',
+            ));
+            return;
+        }
+        _resolve(res.data);
+        this._msgSequence.delete(res.data.id);
+    }
+
+    async fetch<T extends EventHanlder>(params: Record<string, unknown>): Promise<unknown> {
+        return new Promise((resolve, reject) => {
+            params.id = this._curFetchSequenceID;
+            params.event = 'request';
+            this.send(params as SendParams<T>, reject);
+            this._msgSequence.set(this._curFetchSequenceID++, resolve);
+        });
+    };
+};
+
+type TypeForConnector = {
+    server: ServerConnector;
+    client: ClientConnector;
+};
+
+type ConnectorType = keyof TypeForConnector;
+export default (function connectorFactory<T extends ConnectorType>(connectorType: T): TypeForConnector[T] {
+    return (connectorType === 'server' ? new ServerConnector(() => document?.querySelector('iframe')?.contentWindow) : new ClientConnector(() => window?.top)) as TypeForConnector[T];
+})('client');
--- timeline/src/index.tsx
+++ timeline/src/index.tsx
@@ -6,6 +6,7 @@ import './i18n';
 import './index.css';
 import { store } from './store';
 import { NOTIFICATION_HANDLERS } from './interface';
+import connector from './connection';
 
 type CefQueryType = {request: string; onSuccess: (response: string) => void; onFailure: (errorCode: number, errorMessage: string) => void};
 
@@ -41,73 +42,18 @@ window.addEventListener('contextmenu', e => {
     e.stopImmediatePropagation();
 }, true);
 
-const connector = (() => {
-    const send = (args: Record<string, unknown>, reject?: Function): void => {
-        if (!window?.top?.postMessage) {
-            const errMsg = '[connection]: missed postMessage function, please check your iframe element';
-            console.error(errMsg);
-            reject?.(new Error(errMsg));
-            return;
-        }
-        window.top.postMessage(JSON.stringify(args), '*');
-    };
-
-    const _msgSequence: Map<MsgSequence, Function> = new Map();
-    const listeners: Map<Handler, ListenerCb> = new Map();
-    window.onmessage = (e: MessageEvent) => {
-        const res = { ...e };
-        res.data = JSON.parse(e.data);
-        const _resolve = _msgSequence.get(res.data.id);
-        if (_resolve) {
-            _resolve(res.data);
-            _msgSequence.delete(res.data.id);
-        } else {
-            listeners.forEach(cb => cb(res));
-        }
-    };
-
-    type MsgSequence = number;
-    let id: MsgSequence = 0;
-    const fetch = async (args: Record<string, unknown>): Promise<unknown> => {
-        return new Promise((resolve, reject) => {
-            args.id = id;
-            send(args, reject);
-            _msgSequence.set(id++, resolve);
-        });
-    };
-
-    type Handler = number;
-    type ListenerCb = (res: MessageEvent) => void;
-    let _handler = 0;
-    const addListener = (cb: ListenerCb): Handler => {
-        listeners.set(_handler, cb);
-        return _handler++;
-    };
-
-    const removeListener = (handler: Handler): void => {
-        listeners.delete(handler);
-    };
-
-    return {
-        fetch,
-        send,
-        addListener,
-        removeListener,
-    };
-})();
-
 window.request = async (dataSource, params) => {
     const data = await connector.fetch({ event: 'request', remote: dataSource, args: params });
     return (data as any).body;
 };
 
-connector.addListener((e: MessageEvent<{ event: string; dataSource: DataSource; body: Record<string, unknown> }>) => {
-    const res = e.data;
-    if (NOTIFICATION_HANDLERS[res.event] === undefined ||
-        res.body === undefined ||
-        res.dataSource === undefined) {
-        console.error('[notify]', 'Wrong notify format.');
-        return;
-    }
-    NOTIFICATION_HANDLERS[res.event]({ dataSource: res.dataSource, body: res.body });
+Object.entries(NOTIFICATION_HANDLERS).forEach(([ event, callback ]) => {
+    connector.addListener(event, (e: MessageEvent<{ event: string; dataSource: DataSource; body: Record<string, unknown> }>) => {
+        const res = e.data;
+        if (res.body === undefined || res.dataSource === undefined) {
+            console.error('[notify]', 'Wrong notify format.');
+            return;
+        }
+        callback({ dataSource: res.dataSource, body: res.body });
+    });
 });
