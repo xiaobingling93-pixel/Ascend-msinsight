90d1cd9fd893570f3b47922e8103ad7429e23305
insightDeveloper <1390732074@qq.com>
2023-09-12 16:26:27 +0800

    顶部模块名样式调整
    Match-id-9b5708ffdd331ab26f2bfcf1467ef1724830e5be

--- src/components/TabPane.vue
+++ src/components/TabPane.vue
@@ -28,16 +28,13 @@ function toggleTab(index: number): void {
 <template>
     <div class="tab-pane">
         <div class="tab-titles">
-            <nav>
-                <a
-                    v-for="(path, index) in localPaths"
-                    :key="`${index}-${path}`"
-                    @click="() => toggleTab(index)"
-                    :class="index === currentPath && 'active'"
-                >
-                    {{ getModuleName(index) }}
-                </a>
-            </nav>
+            <el-menu class="el-menu-title" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" router>
+                <el-menu-item v-for="(path, index) in localPaths"
+                              :key="`${index}-${path}`"
+                              @click="() => toggleTab(index)"
+                              :class="index === currentPath && 'active'"> {{ getModuleName(index) }}
+                </el-menu-item>
+            </el-menu>
         </div>
         <div class="tab-body">
             <iframe :src="localPaths[currentPath]" ref="moduleRef"></iframe>
@@ -57,6 +54,34 @@ function toggleTab(index: number): void {
     height: var(--header-height);
 }
 
+.el-menu-title {
+  display: flex;
+  border: none;
+  height: 30px;
+  width: 100%;
+  line-height: 30px;
+  border-bottom: var(--border-style);
+}
+
+el-menu-item {
+  background-color: #383838;
+  margin-right: 1px;
+  text-align: center;
+  height: 28px !important;
+  width: 100px;
+  line-height: 30px;
+  user-select: none;
+}
+
+el-menu-item.active {
+  color: #FFD04B !important; /* 你想要的字体颜色 */
+  border-bottom: 2px solid #FFD04B
+}
+
+el-menu-item:hover {
+  background-color: #545C64;
+}
+
 @media (min-height: 1024px) {
     .tab-titles {
         height: 40px;
@@ -65,40 +90,6 @@ function toggleTab(index: number): void {
 
 .tab-body {
     flex-grow: 1;
-}
-
-nav {
-    width: 100%;
-    height: 100%;
-    font-size: 1rem;
-    text-align: left;
-    border-bottom: var(--border-style);
-    display: flex;
-    align-items: flex-end;
-}
-
-nav a.active {
-    color: var(--color-text);
-    background-color: var(--color-background-medium);
-}
-
-nav a:first-of-type {
-    margin-left: 1px;
-}
-
-nav a {
-    display: inline-block;
-    border-radius: 0.5rem 0.5rem 0 0;
-    padding: 0 1rem;
-    border-left: var(--border-style);
-    border-right: var(--border-style);
-    border-top: var(--border-style);
-    height: 90%;
-    min-width: 10rem;
-    text-align: center;
-    margin-right: 1px;
-    text-decoration: none;
-    color: var(--color-text);
 }
 
 @media (hover: hover) {
