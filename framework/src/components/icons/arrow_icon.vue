e257c7056d79077b95f612124bdf576f416efddf
insightDeveloper <1390732074@qq.com>
2023-10-08 10:36:08 +0800

    clean code
    Match-id-5ce89a1d2f8d956dfcd9e425990162f952d36762

--- framework/src/components/icons/arrow_icon.vue
+++ framework/src/components/icons/arrow_icon.vue
@@ -1,19 +1,19 @@
 <template>
-    <svg
-      v-bind="$attrs"
-      t="1693445372132"
-      class="icon"
-      viewBox="0 0 1024 1024"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      p-id="1439"
-      width="100%"
-      height="100%"
-    >
-        <path
-          d="M573.056 752l308.8-404.608A76.8 76.8 0 0 0 820.736 224H203.232a76.8 76.8 0 0 0-61.056 123.392l308.8 404.608a76.8 76.8 0 0 0 122.08 0z"
-          fill="#fff"
-          p-id="1440"
-        ></path>
-    </svg>
+  <svg
+    v-bind="$attrs"
+    t="1693445372132"
+    class="icon"
+    viewBox="0 0 1024 1024"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    p-id="1439"
+    width="100%"
+    height="100%"
+  >
+    <path
+      d="M573.056 752l308.8-404.608A76.8 76.8 0 0 0 820.736 224H203.232a76.8 76.8 0 0 0-61.056 123.392l308.8 404.608a76.8 76.8 0 0 0 122.08 0z"
+      fill="#fff"
+      p-id="1440"
+    ></path>
+  </svg>
 </template>
