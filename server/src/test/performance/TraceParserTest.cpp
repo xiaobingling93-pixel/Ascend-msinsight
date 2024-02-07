0eba426d4f2e98fc8132043d2ba46d06a1f7e240
insightDeveloper <1390732074@qq.com>
2024-02-07 16:28:30 +0800

    增加单卡2G和5GTimeline数据解析的性能用例
    # message auto-generated for no-merge-commit merge:
    merge mac into master
    
    增加单卡2G和5GTimeline数据解析的性能用例
    
    Created-by: insightDeveloper
    
    Match-id-fff4f53978dd25e2414ebc6511aaa708f5f2ee8b

--- server/src/test/performance/TraceParserTest.cpp
+++ server/src/test/performance/TraceParserTest.cpp
@@ -19,24 +19,55 @@ const int TIME_REFERENCE_5S = 5000;
 class TraceParserTest : PerformanceTest {
 };
 
-TEST_F(PerformanceTest, testTraceParser1P100MTime)
+TEST_F(PerformanceTest, testTraceParser1P2GBTime)
 {
     auto start = std::chrono::high_resolution_clock::now();
-    std::string currPath = Dic::FileUtil::GetCurrPath();
-    std::string rootPath = currPath.substr(0, currPath.find_last_of("server") + 1);
+#ifdef _WIN32
+    std::string rootPath = R"(D:\data\2G\ubuntu125_7611_20240206073731998_ascend_pt)";
+#else
+    std::string rootPath = "/home/";
+#endif
     DataBaseManager::Instance().CreatConnectionPool("0",
-        rootPath + R"(/src/test/test_data/test_rank_0/ASCEND_PROFILER_OUTPUT/trace_view.db)");
-    TraceFileParser::Instance().Parse(
-        {rootPath + R"(/src/test/test_data/test_rank_0/ASCEND_PROFILER_OUTPUT/trace_view.json)"}, "0", "");
+        rootPath + R"(/ASCEND_PROFILER_OUTPUT/ascend_insight_data.db)");
+    TraceFileParser::Instance().Parse({rootPath + R"(/ASCEND_PROFILER_OUTPUT/trace_view.json)"}, "0", "");
     while (true) {
         ParserStatus status = ParserStatusManager::Instance().GetParserStatus("0");
         if (status == ParserStatus::FINISH) {
             break;
         }
+        std::chrono::milliseconds duration(50); // 间隔 50 ms
+        std::this_thread::sleep_for(duration);
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto cost = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    EXPECT_LT(cost, TIME_REFERENCE_5S);
+    status.module = TIMELINE_MODULE;
+    status.caseName = ::testing::UnitTest::GetInstance()->current_test_info()->name();
+    status.refTime = std::to_string(TIME_REFERENCE_5S);
+    status.realTime = std::to_string(cost);
+    TraceFileParser::Instance().Reset();
+}
+
+TEST_F(PerformanceTest, testTraceParser1P5GBTime)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+#ifdef _WIN32
+    std::string rootPath = R"(D:\data\5G\ubuntu125_72115_20240206115521705_ascend_pt)";
+#else
+    std::string rootPath = "/home/";
+#endif
+    DataBaseManager::Instance().CreatConnectionPool("0",
+                                                    rootPath + R"(/ASCEND_PROFILER_OUTPUT/ascend_insight_data.db)");
+    TraceFileParser::Instance().Parse({rootPath + R"(/ASCEND_PROFILER_OUTPUT/trace_view.json)"}, "0", "");
+    while (true) {
+        ParserStatus status = ParserStatusManager::Instance().GetParserStatus("0");
+        if (status == ParserStatus::FINISH) {
+            break;
+        }
+        std::chrono::milliseconds duration(50); // 间隔 50 ms
+        std::this_thread::sleep_for(duration);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto cost = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     status.module = TIMELINE_MODULE;
     status.caseName = ::testing::UnitTest::GetInstance()->current_test_info()->name();
     status.refTime = std::to_string(TIME_REFERENCE_5S);
