d72636c2665dd2f7b835a4778853da0285af09cd
insightDeveloper <1390732074@qq.com>
2024-03-28 14:35:00 +0800

    msprof分片文件名字匹配规则修改
    Match-id-2f1562276d82e2449a56d02afa8575f6c6d22f06

--- server/src/defs/CommonDefs.h
+++ server/src/defs/CommonDefs.h
@@ -14,7 +14,7 @@ namespace Dic {
 
     const static std::string KERNEL_DETAIL_REG = R"((kernel_details|op_summary(_slice)?(_[0-9]{1,14}){0,2})\.csv$)";
     const std::string traceViewReg =
-        R"((((trace_view|msprof(_[0-9]{1,15}){1,4})\.json)|(operator_memory|operator_memory_[0-9]{1,14})\.csv)$)";
+    R"((((trace_view|msprof(_slice)?(_[0-9]{1,15}){1,4})\.json)|(operator_memory|operator_memory_[0-9]{1,14})\.csv)$)";
     const std::string clusterReg = R"(cluster_analysis_output$)";
     const std::string DBReg =
             R"((msprof_[0-9]{1,16}|((ascend_pytorch_profiler)(_[0-9]{1,16}){0,1})|cluster_analysis)\.db$)";
