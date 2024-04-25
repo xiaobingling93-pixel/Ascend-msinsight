f9fb8aedbbed202c053ada704b145b6d02081466
insightDeveloper <1390732074@qq.com>
2024-04-25 20:06:55 +0800

    [********][feat]支持算子基础信息展示
    Match-id-b8397f9bdcd7826025e5c259649364e8e03093f8

--- modules/cluster/src/react-app-env.d.ts
+++ modules/cluster/src/react-app-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="react-scripts" />
+// This is necessary for Cluster Module.
--- modules/cluster/src/utils/Logger.ts
+++
@@ -1,4 +0,0 @@
-export type LogLevel = 'debug' | 'info' | 'warn' | 'error';
-export const Logger = (tag: string, msg: string, level?: LogLevel): void => {
-    // wedge platform Logger
-};
