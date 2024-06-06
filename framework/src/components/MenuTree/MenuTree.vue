6634f9a79e011aacbaf971c2e6f298226152b64c
insightDeveloper <1390732074@qq.com>
2024-06-06 20:42:31 +0800

    优化左侧菜单结构、样式
    # message auto-generated for no-merge-commit merge:
    merge 0606-fix-tree into master
    
    修复mac上文件菜单图标显示错位问题
    
    Created-by: insightDeveloper
    
    Match-id-235ef34344db9d725f3a18844e23c4e9679c10df

--- framework/src/components/MenuTree/MenuTree.vue
+++ framework/src/components/MenuTree/MenuTree.vue
@@ -55,33 +55,25 @@ function addRemoteUnderProject(node:any, e: MouseEvent) {
                 <div :class="['contentNode', {activateNode:data.id === activateNodeId}]">
                     <span class="contentBody">
                         <LocalIcon v-if="node.level === 1" style="flex: none"/>
-                        <el-tooltip :content="node.label" v-if="node.level === 1" :effect="isDarkTheme ? 'light' : 'dark'">
-                           <EditableText :tree-node="node" :key="data.id + data.label"></EditableText>
-                        </el-tooltip>
-                        <el-tooltip v-if="node.level === 2" :node-key="node.label" :content="node.label" :effect="isDarkTheme ? 'light' : 'dark'">
-                            <span class="contentNodeText">
-                                {{ node.label }}
-                            </span>
+                        <el-tooltip :content="node.label" :effect="isDarkTheme ? 'light' : 'dark'">
+                            <EditableText  v-if="node.level === 1" :tree-node="node" :key="data.id + data.label"></EditableText>
+                            <span v-else class="contentNodeText">{{ node.label }} </span>
                         </el-tooltip>
                     </span>
-                    <el-tooltip v-if="node.level === 1" content="Import Data" :effect="isDarkTheme ? 'light' : 'dark'">
-                      <span class="addIcon">
-                        <el-icon class="icon-button" @click.stop="addRemoteUnderProject(node, $event)">
-                          <AddIcon />
-                        </el-icon>
-                      </span>
-                    </el-tooltip>
-                    <el-tooltip v-if="node.level === 1" :content="DeleteAll" :effect="isDarkTheme ? 'light' : 'dark'">
-                            <span class="deleteIcon">
-                              <DeletePopConfirm :data="data" :node="node" :is-delete-all="node.level === 1"/>
-                            </span>
-                    </el-tooltip>
-                    <el-tooltip v-if="node.level === 2" :content="DeleteItem" :effect="isDarkTheme ? 'light' : 'dark'">
-                            <span class="deleteIcon">
-                              <DeletePopConfirm :data="data" :node="node" :is-delete-all="node.level === 1"/>
-                            </span>
-                    </el-tooltip>
 
+                    <div class="btn-box">
+                      <el-tooltip v-if="node.level === 1" content="Import Data" :effect="isDarkTheme ? 'light' : 'dark'">
+                          <el-icon class="icon-button" @click.stop="addRemoteUnderProject(node, $event)">
+                            <AddIcon />
+                          </el-icon>
+                      </el-tooltip>
+
+                      <el-tooltip :content="node.level === 1 ? DeleteAll : DeleteItem" :effect="isDarkTheme ? 'light' : 'dark'">
+                        <div class="icon-button">
+                          <DeletePopConfirm :data="data" :node="node" :is-delete-all="node.level === 1"/>
+                        </div>
+                      </el-tooltip>
+                    </div>
                 </div>
             </template>
         </el-tree>
@@ -108,7 +100,7 @@ function addRemoteUnderProject(node:any, e: MouseEvent) {
     justify-content: space-between;
     flex: 1;
     line-height: 20px;
-    width: 100%;
+    width: calc(100% - 18px);
 }
 
 ::v-deep(.contentBody) {
@@ -116,23 +108,8 @@ function addRemoteUnderProject(node:any, e: MouseEvent) {
     align-items: center;
     width: 0;
     flex: 1 1 auto;
-    margin-right: 50px;
     overflow: hidden;
     text-overflow: ellipsis;
-}
-
-::v-deep(.treeDeleteIcon) {
-    position: absolute;
-    right: 0;
-    transition: all 0.5s ease-out;
-}
-
-::v-deep(.contentText) {
-    padding: 4px 0 0 2px;
-    font-size: 14px;
-    color: var(--treeContent-color);
-    line-height: 24px;
-    font-weight: 700;
 }
 
 ::v-deep(.contentNodeText) {
@@ -144,21 +121,19 @@ function addRemoteUnderProject(node:any, e: MouseEvent) {
     white-space: nowrap;
 }
 
-::v-deep(.addIcon) {
-  flex: 0 0 auto;
-  position: absolute;
-  right: 20px;
-  height: 16px;
-  margin-top: 2px;
-}
-
-::v-deep(.deleteIcon) {
-    flex: 0 0 auto;
-    position: absolute;
-    right:0;
-}
 
 ::v-deep(.el-popper.is-dark) {
     color: red;
+}
+
+.btn-box{
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  width: 40px;
+  margin-left: 10px;
+}
+.icon-button{
+  margin-left: 4px;
 }
 </style>
