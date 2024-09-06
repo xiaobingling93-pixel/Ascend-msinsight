3b845a3998804b735c236ba7e43860f09e667aac
insightDeveloper <1390732074@qq.com>
2024-09-06 10:52:15 +0800

    优化左侧项目管理“全部删除”功能
    # message auto-generated for no-merge-commit merge:
    merge 0905-fix-multi-dom into master
    
    【********】左侧项目管理“全部删除“的弹窗渲染过多
    
    Created-by: insightDeveloper
    
    Match-id-20a4eac9196a8488672dc9aeca96ff57efeba2f4

--- framework/src/components/MenuTree/DeletePopConfirm.vue
+++ framework/src/components/MenuTree/DeletePopConfirm.vue
@@ -1,56 +1,51 @@
 <script setup lang="ts">
-import {ref, toRaw} from 'vue';
+import { toRaw } from 'vue';
 import type Node from 'element-plus/es/components/tree/src/model/node';
 import type { TreeNodeType } from '@/components/MenuTree/types';
 import { useDataSources } from '@/stores/dataSource';
 import Delete from './DeleteIcon.vue';
 import useWatchTranslation from '@/hooks/useWatchTranslation';
+import { ElMessageBox } from 'element-plus';
 
 const props = defineProps<{ data: TreeNodeType; isDeleteAll: boolean; node: Node }>();
-const dialogVisible = ref(false);
 const store = useDataSources();
-const [DeleteAll, DeleteItem, Cancel, Confirm, DeleteItemConfirmDescribe, DeleteAllConfirmDescribe] = useWatchTranslation(
-    ['Delete All', 'Delete Item', 'Cancel', 'Confirm', 'DeleteItemConfirmDescribe', 'DeleteAllConfirmDescribe']
-);
+const [DeleteAll, Cancel, Confirm, DeleteItemConfirmDescribe, DeleteAllConfirmDescribe] = useWatchTranslation([
+    'Delete All',
+    'Cancel',
+    'Confirm',
+    'DeleteItemConfirmDescribe',
+    'DeleteAllConfirmDescribe',
+]);
 
 const handleDeleteSingle = () => {
     const parentData = props.node.parent.data;
-    const parentIndex = store.menuTree.findIndex(data => data === toRaw(parentData));
-    const dataIndex = store.menuTree[parentIndex]?.children?.findIndex(data => data === toRaw(props.data));
+    const parentIndex = store.menuTree.findIndex((data) => data === toRaw(parentData));
+    const dataIndex = store.menuTree[parentIndex]?.children?.findIndex((data) => data === toRaw(props.data));
     if (dataIndex !== undefined) {
         store.removeSingle(parentIndex, dataIndex);
     }
 };
 const handleDelete = () => {
-  const current = store.menuTree.findIndex(data => data === toRaw(props.data));
-  store.remove(current);
+    const current = store.menuTree.findIndex((data) => data === toRaw(props.data));
+    store.remove(current);
 };
-const handleDeleteAll = () => {
-  handleDelete();
-  dialogVisible.value = false;
+
+const handleDeleteAllClick = () => {
+    ElMessageBox.confirm(DeleteAllConfirmDescribe.value, {
+        title: DeleteAll.value,
+        confirmButtonText: Confirm.value,
+        cancelButtonText: Cancel.value,
+    }).then(() => {
+        handleDelete();
+    });
 };
 </script>
 
 <template>
-    <el-popconfirm width="200" v-if="!isDeleteAll" :hide-icon="true" @confirm="handleDeleteSingle" :hide-after="0"
-        :title="DeleteItemConfirmDescribe">
+    <Delete v-if="isDeleteAll" @click.stop="handleDeleteAllClick" />
+    <el-popconfirm width="200" v-else :hide-icon="true" @confirm="handleDeleteSingle" :hide-after="0" :title="DeleteItemConfirmDescribe">
         <template #reference>
             <Delete @click.stop />
         </template>
     </el-popconfirm>
-    <Delete v-if="isDeleteAll" @click.stop="dialogVisible = true" />
-    <el-dialog v-model="dialogVisible" :title="DeleteAll" width="20%" :show-close="false" :align-center="true" append-to-body>
-        <span>{{ DeleteAllConfirmDescribe }}</span>
-        <template #footer>
-            <span class="dialog-footer">
-                <el-button @click="dialogVisible = false">{{ Cancel }}</el-button>
-                <el-button type="primary" @click="handleDeleteAll"> {{ Confirm }} </el-button>
-            </span>
-        </template>
-    </el-dialog>
 </template>
-<style>
-.el-dialog__footer button{
-    padding:0 8px;
-}
-</style>
--- framework/src/hooks/useWatchTranslation.ts
+++ framework/src/hooks/useWatchTranslation.ts
@@ -7,7 +7,7 @@ import { t } from '@/i18n';
 import { type TFunctionDetailedResult } from 'i18next';
 import { useSession } from '@/stores/session';
 
-export const useWatchTranslation = (keys: string[]): Array<Ref<string | TFunctionDetailedResult<string>>> => {
+export const useWatchTranslation = (keys: string[]): Array<Ref<string>> => {
     const { session } = useSession();
     const updatedKeys = keys.map((key) => ref(t(key)));
 
@@ -20,7 +20,7 @@ export const useWatchTranslation = (keys: string[]): Array<Ref<string | TFunctio
         },
     );
 
-    return updatedKeys;
+    return updatedKeys as Array<Ref<string>>;
 };
 
 export default useWatchTranslation;
