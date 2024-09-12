0fb066a07a4b432b89e8e9722cbe3f2916e69c9b
insightDeveloper <1390732074@qq.com>
2024-09-12 15:38:21 +0800

    [********][fix]Jupyter链接增加安全判断
    Match-id-b9d4799a64591223a7522229ae1f374a75627976

--- modules/jupyter/src/components/jupyter/Jupyter.tsx
+++ modules/jupyter/src/components/jupyter/Jupyter.tsx
@@ -7,15 +7,29 @@ import type { Session } from '../../entity/session';
 import './Jupyter.css';
 import { loading } from '../Common';
 import { useTranslation } from 'react-i18next';
+import { Layout } from 'ascend-layout';
 
 const index = observer((props: { session: Session }) => {
     const { session } = props;
+    const { t: tHome } = useTranslation();
     const { t } = useTranslation('jupyter');
     if (session.isIpynb && session.ipynbUrl !== '') {
-        return <div className="jupyter-div">
-            <iframe id="jupyter" className="jupyter-iframe" src={session.ipynbUrl}></iframe>
-            <div className="jupyter-tip">{t('Jupyter Tip')}: {session.ipynbUrl}</div>
-        </div>;
+        // http://localhost:端口号/lab/tree/文件相对路径/文件名.ipynb
+        const urlReg = /^http:\/\/localhost:[0-9]{1,10}\/lab\/tree\/.{1,3000}\.ipynb$/;
+        const isSafe = urlReg.test(session.ipynbUrl);
+        if (isSafe) {
+            return <div className="jupyter-div">
+                <iframe id="jupyter" className="jupyter-iframe" src={session.ipynbUrl}></iframe>
+                <div className="jupyter-tip">{t('Jupyter Tip')}: {session.ipynbUrl}</div>
+            </div>;
+        } else {
+            return <Layout>
+                <div style={{ marginLeft: '24px' }}>
+                    <span>{tHome('Error')}: {t('Unsafe Link')} </span>
+                    <span style={{ wordBreak: 'break-all' }}>{session.ipynbUrl}</span>
+                </div>
+            </Layout>;
+        }
     } else {
         return loading;
     }
