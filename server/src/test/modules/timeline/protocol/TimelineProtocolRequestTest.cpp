3f6ca6db23ba3dd098090e04a864353d48686c0d
insightDeveloper <1390732074@qq.com>
2024-12-06 11:42:49 +0800

    增加单元测试4
    # message auto-generated for no-merge-commit merge:
    merge master7 into master
    
    增加单元测试
    
    Created-by: insightDeveloper
    
    Match-id-2dc02f824865a51f61d9d3cf4a1cb3f42a2bf01b

---
+++ server/src/test/modules/timeline/protocol/TimelineProtocolRequestTest.cpp
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+#include <gtest/gtest.h>
+#include "TimelineProtocolRequest.h"
+class TimelineProtocolRequestTest : public ::testing::Test {};
+
+TEST_F(TimelineProtocolRequestTest, TestImportActionParams)
+{
+    Dic::Protocol::ImportActionParams params;
+    params.projectName = "ll";
+    params.projectAction = Dic::Protocol::ProjectActionEnum::UNKNOWN;
+    std::string errorMsg;
+    bool res = params.CommonCheck(errorMsg);
+    EXPECT_EQ(res, false);
+    params.projectAction = Dic::Protocol::ProjectActionEnum::TRANSFER_PROJECT;
+    res = params.CommonCheck(errorMsg);
+    EXPECT_EQ(res, true);
+    res = params.ConvertToRealPath(errorMsg);
+    EXPECT_EQ(res, false);
+    params.path.emplace_back("LLLLLLLLLL");
+    res = params.ConvertToRealPath(errorMsg);
+    EXPECT_EQ(res, false);
+}
+
+TEST_F(TimelineProtocolRequestTest, TestUnitThreadTracesParams)
+{
+    Dic::Protocol::UnitThreadTracesParams params;
+    const uint64_t st = 9;
+    const uint64_t en = 2;
+    params.startTime = st;
+    params.endTime = en;
+    std::string errorMsg;
+    const uint64_t min = 7;
+    bool res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    params.endTime = UINT64_MAX;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    const uint64_t mi = 89;
+    params.endTime = mi;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, true);
+}
+
+TEST_F(TimelineProtocolRequestTest, UnitThreadTracesSummaryParams)
+{
+    Dic::Protocol::UnitThreadTracesSummaryParams params;
+    const uint64_t st = 9;
+    const uint64_t en = 2;
+    params.startTime = st;
+    params.endTime = en;
+    std::string errorMsg;
+    const uint64_t min = 7;
+    bool res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    params.endTime = UINT64_MAX;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    const uint64_t mi = 89;
+    params.endTime = mi;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, true);
+}
+
+TEST_F(TimelineProtocolRequestTest, UnitThreadsParams)
+{
+    Dic::Protocol::UnitThreadsParams params;
+    const uint64_t st = 9;
+    const uint64_t en = 2;
+    params.startTime = st;
+    params.endTime = en;
+    std::string errorMsg;
+    const uint64_t min = 7;
+    bool res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    params.endTime = UINT64_MAX;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    const uint64_t mi = 89;
+    params.endTime = mi;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, true);
+}
+
+TEST_F(TimelineProtocolRequestTest, UnitFlowsParams)
+{
+    Dic::Protocol::UnitFlowsParams params;
+    const uint64_t st = 9;
+    const uint64_t en = 2;
+    params.startTime = st;
+    params.endTime = en;
+    std::string errorMsg;
+    const uint64_t min = 7;
+    bool res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    params.endTime = UINT64_MAX;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    const uint64_t mi = 89;
+    params.endTime = mi;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, true);
+}
+
+TEST_F(TimelineProtocolRequestTest, FlowCategoryEventsParams)
+{
+    Dic::Protocol::FlowCategoryEventsParams params;
+    const uint64_t st = 9;
+    const uint64_t en = 2;
+    params.startTime = st;
+    params.endTime = en;
+    std::string errorMsg;
+    const uint64_t min = 7;
+    bool res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    params.endTime = UINT64_MAX;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    const uint64_t mi = 89;
+    params.endTime = mi;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, true);
+}
+
+TEST_F(TimelineProtocolRequestTest, TestUnitCounterParams)
+{
+    Dic::Protocol::UnitCounterParams params;
+    const uint64_t st = 9;
+    const uint64_t en = 2;
+    params.startTime = st;
+    params.endTime = en;
+    std::string errorMsg;
+    const uint64_t min = 7;
+    bool res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    params.endTime = UINT64_MAX;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    const uint64_t mi = 89;
+    params.endTime = mi;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, true);
+}
+
+TEST_F(TimelineProtocolRequestTest, TestUnitThreadsOperatorsParams)
+{
+    Dic::Protocol::UnitThreadsOperatorsParams params;
+    const uint64_t st = 9;
+    const uint64_t en = 2;
+    params.startTime = st;
+    params.endTime = en;
+    std::string errorMsg;
+    const uint64_t min = 7;
+    bool res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    params.endTime = UINT64_MAX;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+    const uint64_t mi = 89;
+    params.endTime = mi;
+    res = params.CheckParams(min, errorMsg);
+    EXPECT_EQ(res, false);
+}
