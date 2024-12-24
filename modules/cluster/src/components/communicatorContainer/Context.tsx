d2cadbef1ebc96287416003b71a57934ad0844fd
insightDeveloper <1390732074@qq.com>
2024-12-24 15:31:38 +0800

    支持域维度层次化分析性能差异-part2
    # message auto-generated for no-merge-commit merge:
    merge AR-rank--2 into master
    
    支持域维度层次化分析性能差异-part2
    
    Created-by: insightDeveloper
    
    Match-id-8baad8682e2ff67b01c39312d7e2cbc79c8b32cf

---
+++ modules/cluster/src/components/communicatorContainer/Context.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+import React, { createContext, useContext, useState } from 'react';
+import { ParallelismType } from '../../utils/interface';
+
+interface ParallelSwitchConditionsContextType {
+    dyeingMode: string;
+    dyeingStep: number;
+    parallelTypeList: ParallelismType[];
+    setDyeingMode: (value: string) => void;
+    setDyeingStep: (value: number) => void;
+    setParallelTypeList: (values: ParallelismType[]) => void;
+    reset: () => void;
+}
+const ParallelSwitchConditionsContext = createContext<ParallelSwitchConditionsContextType | undefined>(undefined);
+export function ParallelSwitchConditionsProvider({ children }: { children: React.ReactNode }): JSX.Element {
+    const [dyeingMode, setDyeingMode] = useState('None');
+    const [dyeingStep, setDyeingStep] = useState(0.03);
+    const [parallelTypeList, setParallelTypeList] = useState<ParallelismType[]>([]);
+
+    const reset = (): void => {
+        setDyeingMode('None');
+        setDyeingStep(0.03);
+        setParallelTypeList([]);
+    };
+    return (
+        <ParallelSwitchConditionsContext.Provider value={{
+            dyeingMode,
+            setDyeingMode,
+            dyeingStep,
+            setDyeingStep,
+            parallelTypeList,
+            setParallelTypeList,
+            reset,
+        }}>
+            {children}
+        </ParallelSwitchConditionsContext.Provider>
+    );
+}
+
+export function useParallelSwitchConditions(): ParallelSwitchConditionsContextType {
+    const context = useContext(ParallelSwitchConditionsContext);
+    if (!context) {
+        throw new Error('missing required parameters in ParallelSwitchConditionsProvider');
+    }
+    return context;
+}
--- modules/cluster/src/utils/interface.ts
+++ modules/cluster/src/utils/interface.ts
@@ -1,6 +1,12 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
  */
+
+export interface ErrorInfo {
+    code: number;
+    message?: string;
+}
+
 export interface StringMap {
     [prop: string]: string;
 }
@@ -17,4 +23,75 @@ export interface optionDataType {
 
 export interface optionMapDataType {
     [props: string]: optionDataType[];
+}
+
+export interface GetParallelStrategyRes {
+    algorithm: 'megatron-lm(tp-cp-ep-dp-pp)' | 'megatron-lm(tp-cp-pp-dp-ep)';
+    level: string;
+    dpSize: number;
+    ppSize: number;
+    tpSize: number;
+    epSize: number;
+    cpSize: number;
+}
+
+export type SetParallelStrategyParams = Omit<GetParallelStrategyRes, 'level'>;
+
+export type ParallelismType = 'ep' | 'dp' | 'cp' | 'pp' | 'tp';
+export interface ParallelismArrangementParams extends SetParallelStrategyParams {
+    dimension: 'ep-dp' | 'ep-dp-cp' | 'ep-dp-cp-pp' | 'ep-dp-cp-pp-tp';
+}
+
+export interface IndicatorsItem {
+    name: string;
+    key: string;
+    rendering: boolean;
+    chart: 'bar' | 'line';
+    stack: string;
+    yAxisType: 'time' | 'ratio';
+    visible: boolean;
+}
+
+export interface ConnectionsItem {
+    type: ParallelismType;
+    list: number[];
+    group: string[];
+}
+
+export interface DomainItem {
+    type: ParallelismType;
+    list: number[];
+}
+
+export interface ArrangementItem {
+    index: number;
+    name: string;
+    position: { x: number; y: number };
+    indicator: Record<string, number>;
+    attribute: {
+        [K in ParallelismType as `${K}Index`]: number;
+    };
+}
+
+export interface ParallelismArrangementResult {
+    size: number;
+    arrangements: ArrangementItem[];
+    indicators: IndicatorsItem[];
+    connections: ConnectionsItem[];
+    domains: string[];
+}
+
+export interface GetParallelismPerformanceData extends ParallelismArrangementParams {
+    indexList?: number[];
+    orderBy?: string;
+    step?: string;
+}
+
+export interface PerformanceDataItem {
+    [key: string]: number;
+    index: number;
+}
+export interface GetParallelismPerformanceRes {
+    performance: PerformanceDataItem[];
+    advice: string[];
 }
