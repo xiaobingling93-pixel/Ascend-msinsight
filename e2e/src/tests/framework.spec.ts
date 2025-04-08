51348034e01880710724e0e878d7d7a6bcd5e04c
insightDeveloper <1390732074@qq.com>
2025-04-08 15:58:15 +0800

    删除 GUI 非必要用例
    # message auto-generated for no-merge-commit merge:
    merge GUI-0407 into master
    
    删除非必要 GUI 用例
    
    Created-by: insightDeveloper
    
    Match-id-052fbb35894638b15faad923b0982a1f6d2d9482

--- e2e/src/tests/framework.spec.ts
+++ e2e/src/tests/framework.spec.ts
@@ -4,25 +4,11 @@
 
 import { test, expect } from '@playwright/test';
 import { FrameworkPage } from '@/page-object';
-import { clearAllData, importData } from '@/utils';
 
 test.describe('Framework', () => {
     test.beforeEach(async ({ page }) => {
         const frameworkPage = new FrameworkPage(page);
         await frameworkPage.goto();
-    });
-
-    // 导入项目
-    test('import data', async ({ page }) => {
-        const frameworkPage = new FrameworkPage(page);
-        await frameworkPage.goto();
-        await importData(page);
-    });
-
-    // 清空项目
-    test('clear all data', async ({ page }) => {
-        await importData(page);
-        await clearAllData(page);
     });
 
     // 切换主题
