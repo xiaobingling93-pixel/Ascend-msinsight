2f8e1983232f14c5c50b7fff714a73b29b307576
insightDeveloper <1390732074@qq.com>
2025-11-07 10:26:38 +0800

    【开发自提】Timeline服务化视图无法点击切换 & 算子调优 roofline选框背景优化
    Match-id-02a8563424f5c1e698f75a2b20ab9f98c634adc7

--- modules/compute/src/components/detail/Roofline/Index.tsx
+++ modules/compute/src/components/detail/Roofline/Index.tsx
@@ -12,19 +12,41 @@ import { RooflineChartGroup } from './RooflineChart';
 import { queryRoofline } from '../../../components/RequestUtils';
 import { Hit } from 'ascend-utils';
 import type { TFunction } from 'i18next';
-import styled from '@emotion/styled';
 
+/**
+ * 表示原始数据的接口
+ * @interface IOriginData
+ * @property {string} soc - 系统操作条件。
+ * @property {string} advice - 建议或说明。
+ * @property {IOriginRooflineChart[]} data - 包含原始图表数据的数组。
+ */
 export interface IOriginData {
     soc: string;
     advice: string;
     data: IOriginRooflineChart[];
 }
 
+/**
+ * 表示原始图表数据的接口
+ * @interface IOriginRooflineChart
+ * @property {string} title - 图表的标题。
+ * @property {IOriginRoofline[]} rooflines - 包含原始线条数据的数组。
+ */
 interface IOriginRooflineChart {
     title: string;
     rooflines: IOriginRoofline[];
 }
 
+/**
+ * 表示原始线条数据的接口
+ * @interface IOriginRoofline
+ * @property {string} bw - 带宽。
+ * @property {string} computility - 计算能力。
+ * @property {[string, string]} point - 数据点，包含两个字符串。
+ * @property {string} computilityName - 计算能力的名称。
+ * @property {string} bwName - 带宽的名称。
+ * @property {string} ratio - 比率。
+ */
 interface IOriginRoofline {
     bw: string;
     computility: string;
@@ -34,28 +56,56 @@ interface IOriginRoofline {
     ratio: string;
 }
 
+/**
+ * 处理后的数据的接口
+ * @interface IData
+ * @property {string} soc - 系统操作条件。
+ * @property {string} advice - 建议或说明。
+ * @property {IRooflineChart[]} data - 包含处理后的图表数据的数组。
+ */
 interface IData {
     soc: string;
-    advice: string ;
+    advice: string;
     data: IRooflineChart[];
 }
 
+/**
+ * 处理后的图表数据的接口
+ * @interface IRooflineChart
+ * @property {string} title - 图表的标题。
+ * @property {IRoofline[]} rooflines - 包含处理后的线条数据的数组。
+ */
 export interface IRooflineChart {
     title: string;
     rooflines: IRoofline[];
 }
 
+/**
+ * 处理后的线条数据的接口
+ * @interface IRoofline
+ * @property {number} bw - 带宽，数值类型。
+ * @property {number} computility - 计算能力，数值类型。
+ * @property {Point} point - 数据点，包含两个数值。
+ * @property {string} computilityName - 计算能力的名称。
+ * @property {string} bwName - 带宽的名称。
+ * @property {string} ratio - 比率。
+ */
 export interface IRoofline {
     bw: number;
     computility: number;
     point: Point;
-    bwName: string;
     computilityName: string;
+    bwName: string;
     ratio: string;
 }
 
+/**
+ * 二维点的类型，包含两个数值
+ * @typedef {Array<number>} Point
+ * @property {number} [0] - 点的 x 坐标。
+ * @property {number} [1] - 点的 y 坐标。
+ */
 export type Point = [number, number];
-
 interface ITab {
     label: string;
     key: string;
@@ -110,12 +160,6 @@ function getTabItems(data: IData, tDetails: TFunction): Tab[] {
     });
 }
 
-const CustomTabs = styled(Tabs)`
-  .ant-tabs-tab {
-    background-color: transparent !important;
-  }
-`;
-
 const index = observer(({ session }: { session: Session }): JSX.Element => {
     const { t: tDetails } = useTranslation('details');
     const [data, setData] = useState<IData>(defaultData);
@@ -154,7 +198,7 @@ const index = observer(({ session }: { session: Session }): JSX.Element => {
     return data?.data?.length > 0
         ? (
             <CollapsiblePanel title={tDetails('Roofline')} collapsible>
-                <CustomTabs items={items} data-testId={'rooflineChart'} />
+                <Tabs items={items} type="card" data-testId={'rooflineChart'} />
                 {data?.advice?.length > 0 && <Hit text={data.advice} type={'alarm'} data-testId={'rooflineAdvice'}/>}
             </CollapsiblePanel>
         )
