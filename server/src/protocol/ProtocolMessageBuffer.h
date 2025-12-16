22581b2f7cc495588149d8978f121949dc21eed8
insightDeveloper <1390732074@qq.com>
2025-12-16 15:41:52 +0800

    版权修改
    # message auto-generated for no-merge-commit merge:
    merge banquan into master
    
    版权修改
    
    Created-by: insightDeveloper
    
    Match-id-6ca89420da77d843b3172dd6cf0860802bcc4adf

--- server/src/protocol/ProtocolMessageBuffer.h
+++ server/src/protocol/ProtocolMessageBuffer.h
@@ -1,6 +1,19 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2012-2022. All rights reserved.
- * Description: declaration for request message buffer
+ * -------------------------------------------------------------------------
+ * This file is part of the MindStudio project.
+ * Copyright (c) 2025 Huawei Technologies Co.,Ltd.
+ *
+ * MindStudio is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * -------------------------------------------------------------------------
  */
 
 #ifndef DATA_INSIGHT_CORE_SERVER_MESSAGE_BUFFER_H
