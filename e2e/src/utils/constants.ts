dae81ea63584374878ebdcbba47bd38d1b7b69b0
qinyyy <490012999@qq.com>
2026-03-20 16:04:48 +0800

    更换GUI数据地址
    Co-authored-by: qiny<490012999@qq.com>

--- e2e/src/utils/constants.ts
+++ e2e/src/utils/constants.ts
@@ -23,26 +23,26 @@ const getCurrentFilePath = (): string => {
     return __dirname;
 };
 
-const DATA_PATH = 'C:';
+const DATA_PATH = 'C:\\msinsight-quick-start-demo\\GUI-test-data';
 
 export const FilePath = {
-    TEXT: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\single-node\\level2_text`,
-    TEXT_RANK_0: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\single-node\\level2_text\\rank_0_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    TEXT_RANK_2: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\single-node\\level2_text\\rank_2_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    DB: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\single-node\\level2`,
-    DB_RANK_0: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\single-node\\level2\\rank_0_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    DB_RANK_1: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\single-node\\level2\\rank_1_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    OP_SIMULATOR_BIN: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\operator\\msprof-op-simulator\\visualize_data.bin`,
-    OP_SIMULATOR_JSON: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\operator\\msprof-op-simulator\\trace.json`,
-    MULTI_NODES: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\multi-node\\MultiProfLevel2MemoryUB_db`,
-    MULTI_NODES_NODE_0_RANK_0: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\node1_2166651_20240619060505060_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    MULTI_NODES_NODE_0_RANK_1: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\node1_2166652_20240619060505059_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    MULTI_NODES_NODE_1_RANK_0: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\ubuntu2204_1660963_20240619060440181_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    MULTI_NODES_NODE_1_RANK_1: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\ubuntu2204_1660964_20240619060440179_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
-    DETAILS: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\operator\\msprof-op\\details\\visualize_data.bin`,
-    DETAILS_ROOFLINE: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\operator\\msprof-op\\roofline\\visualize_data.bin`,
-    DETAILS_CORE: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\operator\\msprof-op\\core_inter_load\\visualize_data.bin`,
-    REINFORCEMENT_LEARNING: `${DATA_PATH}\\MindStudio-Insight-GUI-Data\\training\\reinforcement-learning\\verl`,
+    TEXT: `${DATA_PATH}\\training\\single-node\\level2_text`,
+    TEXT_RANK_0: `${DATA_PATH}\\training\\single-node\\level2_text\\rank_0_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    TEXT_RANK_2: `${DATA_PATH}\\training\\single-node\\level2_text\\rank_2_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    DB: `${DATA_PATH}\\training\\single-node\\level2`,
+    DB_RANK_0: `${DATA_PATH}\\training\\single-node\\level2\\rank_0_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    DB_RANK_1: `${DATA_PATH}\\training\\single-node\\level2\\rank_1_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    OP_SIMULATOR_BIN: `${DATA_PATH}\\operator\\msprof-op-simulator\\visualize_data.bin`,
+    OP_SIMULATOR_JSON: `${DATA_PATH}\\operator\\msprof-op-simulator\\trace.json`,
+    MULTI_NODES: `${DATA_PATH}\\training\\multi-node\\MultiProfLevel2MemoryUB_db`,
+    MULTI_NODES_NODE_0_RANK_0: `${DATA_PATH}\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\node1_2166651_20240619060505060_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    MULTI_NODES_NODE_0_RANK_1: `${DATA_PATH}\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\node1_2166652_20240619060505059_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    MULTI_NODES_NODE_1_RANK_0: `${DATA_PATH}\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\ubuntu2204_1660963_20240619060440181_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    MULTI_NODES_NODE_1_RANK_1: `${DATA_PATH}\\training\\multi-node\\MultiProfLevel2MemoryUB_db\\ubuntu2204_1660964_20240619060440179_ascend_pt\\ASCEND_PROFILER_OUTPUT`,
+    DETAILS: `${DATA_PATH}\\operator\\msprof-op\\details\\visualize_data.bin`,
+    DETAILS_ROOFLINE: `${DATA_PATH}\\operator\\msprof-op\\roofline\\visualize_data.bin`,
+    DETAILS_CORE: `${DATA_PATH}\\operator\\msprof-op\\core_inter_load\\visualize_data.bin`,
+    REINFORCEMENT_LEARNING: `${DATA_PATH}\\training\\reinforcement-learning\\verl`,
 
     TEXT_330: 'D:\\GUI_TEST_DATA 路徑测试\\系统调优\\test_level1_PipeUtilization\\profiling_data', // 给 Memory 模块使用
     TEXT_330_RANK_0: 'D:\\GUI_TEST_DATA 路徑测试\\系统调优\\test_level1_PipeUtilization\\profiling_data\\ubuntu2204_3929556_20250411014455476_ascend_pt\\ASCEND_PROFILER_OUTPUT',
@@ -55,7 +55,7 @@ export const FilePath = {
     JOINT_DATA: '/home/profiler_performance/task',
 
     // 预冒烟数据
-    SMOKE_DATA: path.join(getCurrentFilePath(), '..', '..', '..', 'test', 'st', 'level2')
-}
+    SMOKE_DATA: path.join(getCurrentFilePath(), '..', '..', '..', 'test', 'st', 'level2'),
+};
 
 export const WEBSOCKET_URL = 'ws://localhost:9000/';
